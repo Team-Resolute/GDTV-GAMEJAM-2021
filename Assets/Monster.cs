c79c3b1f17de1ee62c5c856af5cbca36aa8f1dbf
Anthony Juarez <lordrahvin@gmail.com>
2021-05-31 00:24:25 -0700

    Starting work on the Monster

---
+++ Assets/Monster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    private float hitpoints = 5f;
+    private enum Status {None, MovingRight, MovingLeft, Attacking}
+
+    private Transform rightShoulder;
+    private Transform leftShoulder;
+    
+    void Start()
+    {
+            
+    }
+
+    
+    void Update()
+    {
+            
+    }
+}
