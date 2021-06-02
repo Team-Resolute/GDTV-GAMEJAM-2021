0dd8b4d4079cdac9b0517d7ed6452c2bb73dff31
Anthony Juarez <lordrahvin@gmail.com>
2021-06-02 00:35:53 -0700

    Scene Shifts working in Illusia
    No triggers yet, but you can see the background change when you press comma (",") key.  Music will change too once we plug it in.

--- Assets/Scripts/SceneShifter.cs
+++ Assets/Scripts/SceneShifter.cs
@@ -1,28 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class SceneShifter : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer quadToShift = default;
-    [SerializeField] private List<Material> shiftedMaterials = new List<Material>();
+    [SerializeField] private List<GameObject> shifts = new List<GameObject>();
+    
     [SerializeField] private List<AudioClip> shiftedAudio = new List<AudioClip>();
+    
+    [SerializeField] private List<GameObject> shiftedLight = new List<GameObject>();
     [SerializeField] private AudioSource musicPlayer = default;
+    private GameObject currentShift = default;
+    private GameObject currentLight = default;
     
     int shiftNumber = 0;
     
     void Start()
     {
-        if (shiftedMaterials.Count > 0)
+        if (shifts.Count > 0)
         {
-            quadToShift.material = shiftedMaterials[0];
-            
+            foreach (GameObject shift in shifts)
+            {
+                shift.SetActive(false);
+            }
+
+            currentShift = shifts[0];
+            currentShift.SetActive(true);
             shiftNumber++;
         }
 
         if (musicPlayer && shiftedAudio.Count > 0)
         {
             musicPlayer.clip = shiftedAudio[0];
+            musicPlayer.Play();
+        }
+        
+        if (shiftedLight.Count > 0)
+        {
+            shiftedLight[0].SetActive(true);
+            currentLight = shiftedLight[0];
         }
         
     }
@@ -37,16 +54,27 @@ public class SceneShifter : MonoBehaviour
 
     public void Shift()
     {
-        if (shiftNumber < shiftedMaterials.Count)
+        if (shiftNumber < shifts.Count)
         {
-            quadToShift.material = shiftedMaterials[shiftNumber];
+            currentShift.SetActive(false);
+            currentShift = shifts[shiftNumber];
+            currentShift.SetActive(true);
         }
         
         if (shiftNumber < shiftedAudio.Count)
         {
+            musicPlayer.Stop();
             musicPlayer.clip = shiftedAudio[shiftNumber];
+            musicPlayer.Play();
         }
 
+        if (shiftNumber < shiftedLight.Count)
+        {
+            currentLight.SetActive(false);
+            currentLight = shiftedLight[shiftNumber];
+            currentLight.SetActive(true);
+        }
+        
         shiftNumber++;
 
     }
