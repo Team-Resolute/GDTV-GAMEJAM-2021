1a0627668e0ac83a7e18e86647e445150f8e7911
Yohael <yohanan.escandell@gmail.com>
2021-06-03 17:15:13 +0100

    [Fix] Buttons on end Game Win Scren

--- Assets/EndMenu.cs
+++ Assets/EndMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Sound;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EndMenu : MonoBehaviour
 {
-    [SerializeField] private Image buttonHider = default;
+    [SerializeField] private GameObject buttonHider = default;
     [SerializeField] private float revealDelay = 12f;
     void Start()
     {
@@ -23,12 +24,14 @@ public class EndMenu : MonoBehaviour
     public void RestartGame()
     {
         LevelChanger.Instance.ChangeScene(Level.StartMenu);
+        SoundManager.PlaySound(SoundManager.Sound.ClickButton);
     }
 
     public void QuitGame()
     {
         DialogueManager.Instance.NewDialogue();
         DialogueManager.Instance.AddDialogueSpeech(Speaker.MinorCharacter,"Thank you for playing!");
+        SoundManager.PlaySound(SoundManager.Sound.ClickButton);
         Invoke(nameof(Quit), 2f);
     }
 
